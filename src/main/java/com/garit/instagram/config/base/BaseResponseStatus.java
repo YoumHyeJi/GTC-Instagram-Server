bfeecc034e0bce4c02d1741e5c24e4671fb1635c
YoumHyeJi <yheji0321@gmail.com>
2022-07-27 18:31:37 +0900

    프로젝트 초기 세팅 - build.gradle - application.yml, application-prod.yml, application-dev.yml - BaseException, BaseResponse, BaseResponseCode - TestController

---
+++ src/main/java/com/garit/instagram/config/base/BaseResponseStatus.java
@@ -0,0 +1,21 @@
+package com.garit.instagram.config.base;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다.");
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
