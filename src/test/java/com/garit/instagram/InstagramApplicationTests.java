1495afc54d336660b8b0c9294d839a96c9720ef7
YoumHyeJi <50475178+YoumHyeJi@users.noreply.github.com>
2022-08-07 19:49:47 +0900

    Update InstagramApplicationTests.java

--- src/test/java/com/garit/instagram/InstagramApplicationTests.java
+++ src/test/java/com/garit/instagram/InstagramApplicationTests.java
@@ -3,10 +3,10 @@ package com.garit.instagram;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class InstagramApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
